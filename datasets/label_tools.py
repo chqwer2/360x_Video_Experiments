0027b055a1ea3b2278a8d37bd0df50e468b2f668
chqwer2 <ch981129@126.com>
2023-02-25 23:30:56 +0800

    process the files structure

---
+++ datasets/label_tools.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+
+
+## ------------------- label conversion tools ------------------ ##
+def labels2cat(label_encoder, list):
+    return label_encoder.transform(list)
+
+def labels2onehot(OneHotEncoder, label_encoder, list):
+    return OneHotEncoder.transform(label_encoder.transform(list).reshape(-1, 1)).toarray()
+
+def onehot2labels(label_encoder, y_onehot):
+    return label_encoder.inverse_transform(np.where(y_onehot == 1)[1]).tolist()
+
+def cat2labels(label_encoder, y_cat):
+    return label_encoder.inverse_transform(y_cat).tolist()
+
