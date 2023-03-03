1c4cfeec93c9c41fd154817799c0eae93c1cee33
Calvin Chen <calvin.chen@asml.com>
2023-03-03 18:21:54 +0800

    fix scripts

--- SSL_Sound/vis_scripts/vis_video_itd.py
+++ SSL_Sound/vis_scripts/vis_video_itd.py
@@ -192,7 +192,9 @@ def test(args, device):
         csv_file = csv.DictReader(open(pr.list_vis, 'r'), delimiter=',')
         for row in csv_file:
             print("previous row:", row)
-            if row.endswith("audio") or row.endswith("meta") or row.endswith("frames") or row.endswith("json"):
+            # if row.endswith("audio") or row.endswith("meta") or row.endswith("frames") or row.endswith("json"):
+            #     continue
+            if "mp4" not in row['path']:
                 continue
             print("row:", row)
 
